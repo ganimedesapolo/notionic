bf14cb776ecffc45251fe1e1c587e86c7229d150
ganimedesapolo <diegoerleyduran@yahoo.com>
2018-10-02 15:11:09 -0500

    primer envio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,49 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {NotesService} from '../services/notes.service';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {DetailPage} from '../pages/detail/detail';
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8upnf8aOApsh30POKjftH15jtvkNCNOA",
+  authDomain: "notionic-297dc.firebaseapp.com",
+  databaseURL: "https://notionic-297dc.firebaseio.com",
+  storageBucket: "notionic-297dc.appspot.com",
+  messagingSenderId: '535543551186'
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NotesService
   ]
 })
 export class AppModule {}
---
+++ src/pages/detail/detail.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NotesService } from '../../services/notes.service';
+
+/**
+ * Generated class for the DetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-detail',
+  templateUrl: 'detail.html',
+})
+export class DetailPage {
+  note:any = {id:null,title:null,description:null};
+   id = null;
+   show=true;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public notesService:NotesService) {
+  this.id = navParams.get('id');
+  if(this.id!=0){
+    notesService.getNote(this.id).valueChanges().subscribe(
+      note => {
+        this.note = note
+      } 
+    )
+  }
+}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailPage');
+  }
+
+  addNote(){
+    if(this.id!=0){
+      this.notesService.editNote(this.note);
+      alert("Nota editada con exito");
+     }else{
+      this.note.id=Date.now();
+      this.notesService.createNote(this.note);
+      alert("Nota creada con exito");
+      }
+      this.navCtrl.pop();
+  }
+
+
+  deleteNote(){
+    this.show =false;
+    this.notesService.deleteNote(this.note);
+    alert("Nota eliminada con exito");
+    this.navCtrl.pop();
+  }
+
+}
